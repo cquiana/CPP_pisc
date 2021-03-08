a5f972970c8bc2a7d57f8af5e2230395d7a289fa
Albert <atulyakov@gmail.com>
2021-03-08 23:04:27 +0300

    ex00

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: cquiana <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 22:08:48 by cquiana           #+#    #+#             */
-/*   Updated: 2021/03/08 22:37:07 by cquiana          ###   ########.fr       */
+/*   Updated: 2021/03/08 23:01:57 by cquiana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,21 @@
 
 int		main(int ac, char **av)
 {
+	int i = 1;
+	int j = 0;
+
 	if (ac < 2) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << "\n";
-		std::cout << av[0] << "\n";
+	} else {
+		while (i < ac) {
+			j = 0;
+			while (av[i][j] != '\0') {
+				std::cout << (char)toupper(av[i][j]);
+				j++;
+			}
+			i++;
+		}
+		std::cout << "\n";
 	}
 	return 0;
 }
