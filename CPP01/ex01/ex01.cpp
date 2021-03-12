f502c028c0f1d8eda3bf73076a42cd1bfdb88e56
Albert <atulyakov@gmail.com>
2021-03-12 10:38:55 +0300

    d01ex01

--- CPP01/ex01/ex01.cpp
+++ CPP01/ex01/ex01.cpp
@@ -1,32 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Pony.hpp                                           :+:      :+:    :+:   */
+/*   ex01.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cquiana <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/12 06:58:44 by cquiana           #+#    #+#             */
-/*   Updated: 2021/03/12 10:33:37 by cquiana          ###   ########.fr       */
+/*   Created: 2021/03/12 10:32:40 by cquiana           #+#    #+#             */
+/*   Updated: 2021/03/12 10:34:26 by cquiana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#ifndef PONY_HPP
-#define PONY_HPP
 
 #include <iostream>
 #include <string>
 
-class Pony
+void memoryLeak()
 {
-private:
-	int _age;
-	std::string _name;
-	std::string _color;
-public:
-	Pony(int age, std::string name, std::string color);
-	~Pony();
-
-	void getInfo();
-};
-
-#endif
+	std::string* panther = new std::string("String panther");
+	std::cout << *panther << std::endl;
+	delete panther;
+}
--- CPP01/ex01/main.cpp
+++ CPP01/ex01/main.cpp
@@ -1,32 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Pony.hpp                                           :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cquiana <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/12 06:58:44 by cquiana           #+#    #+#             */
-/*   Updated: 2021/03/12 10:33:37 by cquiana          ###   ########.fr       */
+/*   Created: 2021/03/12 10:35:10 by cquiana           #+#    #+#             */
+/*   Updated: 2021/03/12 10:38:18 by cquiana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PONY_HPP
-#define PONY_HPP
-
 #include <iostream>
 #include <string>
+#include <unistd.h>
 
-class Pony
+void memoryLeak()
 {
-private:
-	int _age;
-	std::string _name;
-	std::string _color;
-public:
-	Pony(int age, std::string name, std::string color);
-	~Pony();
+	std::string* panther = new std::string("String panther");
+	std::cout << *panther << std::endl;
+	// delete panther;
+}
 
-	void getInfo();
-};
+int main(void)
+{
 
-#endif
+	memoryLeak();
+	sleep(100);
+	return 0;
+}
